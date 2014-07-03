e1ba201d0fe3695b190dcfce11d988c7ce99cce5
takshil <cyril.clemenceau@gmail.com>
2014-07-03 11:12:30 +0200

    first commit

---
+++ Balle/src/fr/formation/balle/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.formation.balle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new SurfaceViewBalle(this));
+	}
+}
