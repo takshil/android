bd473150edd8a0227376f1f42f9dbd5c99a935a4
Cyril Clemenceau <cyril@cypy.fr>
2017-03-25 15:29:39 +0100

    commit

--- MyFirstApplication/gen/fr/formation/firstapplication/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.formation.firstapplication;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- MyFirstApplication/src/fr/formation/firstapplication/AppPrincipale.java
+++
@@ -1,7 +0,0 @@
-package fr.formation.firstapplication;
-
-import android.app.Application;
-
-public class AppPrincipale extends Application {
-
-}
